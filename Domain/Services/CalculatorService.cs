efe9a2526018935692ec0f5af785dfbaa56541fc
Sheldon Boshoff <boshoffsheldon@gmail.com>
2021-02-18 09:42:23 +0000

    Trimmed CalculatorService to use DataTable

--- Domain/Services/CalculatorService.cs
+++ Domain/Services/CalculatorService.cs
@@ -1,57 +1,19 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Calculator.API.Domain.Services.Interfaces;
+using System.Web;
+using System.Data;
 
 namespace Calculator.API.Domain.Services
 {
     public class CalculatorService : ICalculatorService {
         public async Task<string> calculate(string expression)
         {
-            string output;
-            string[] values = expression.Split(' ');
-
-            double first = float.Parse(values[0]);
-            double second = float.Parse(values[2]);
-            string op = values[1];
-
-            double result = 0;
-
-            switch (op)
-            {
-                case "+":
-                    result = first + second;
-                    break;
-                case "-":
-                    result = first - second;
-                    break;
-                case "*":
-                    result = first * second;
-                    break;
-                case "/":
-                    if (second != 0)
-                    {
-                        result = first / second;
-                    }
-                    else
-                    {
-                        result = double.NaN;
-                    }
-                    break;
-                default:
-                    result = 0;
-                    break;
+            if (expression == null) {
+                return "N/A";
             }
 
-            if (result != double.NaN)
-            {
-                output = result.ToString();
-            }
-            else
-            {
-                output = "NaN";
-            }
-
-            return output;
+            return new DataTable().Compute(expression, "").ToString();
         }
     }
 }
